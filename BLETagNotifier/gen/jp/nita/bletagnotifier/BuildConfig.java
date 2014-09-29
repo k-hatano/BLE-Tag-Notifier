ae8f2f139f335365a0842b529abfa009142ecf82
kenta <kent.ruffle.mgj626@gmail.com>
2014-09-29 18:05:01 +0900

    設定画面で訳がわからないことになったなんとかして

---
+++ BLETagNotifier/gen/jp/nita/bletagnotifier/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package jp.nita.bletagnotifier;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ BLETagNotifier/src/jp/nita/bletagnotifier/MainActivity.java
@@ -0,0 +1,29 @@
+package jp.nita.bletagnotifier;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainActivity extends Activity implements OnClickListener {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		findViewById(R.id.preferences).setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v) {
+		if(v==findViewById(R.id.preferences)){
+			Intent intent = new Intent(this,SettingsActivity.class);
+			startActivity(intent);
+		}
+	}
+}
