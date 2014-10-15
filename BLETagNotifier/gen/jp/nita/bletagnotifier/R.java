2829f299f58ce54b585b81d39a20ae114fb2b201
k-hatano <kent.ruffle.mgj626@gmail.com>
2014-10-16 02:00:53 +0900

    テンプレなんざクソ食らえ

--- BLETagNotifier/gen/jp/nita/bletagnotifier/R.java
+++ BLETagNotifier/gen/jp/nita/bletagnotifier/R.java
@@ -14,23 +14,23 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int history=0x7f070000;
-        public static final int preferences=0x7f070001;
+        public static final int history=0x7f060000;
+        public static final int listView=0x7f060003;
+        public static final int preferences=0x7f060001;
+        public static final int scrollView1=0x7f060002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int activity_settings=0x7f030001;
     }
     public static final class string {
-        public static final int app_name=0x7f050000;
-        public static final int ble_tag_settings=0x7f050005;
-        public static final int history=0x7f050003;
-        public static final int incoming_call=0x7f050009;
-        public static final int notification_settings=0x7f050008;
-        public static final int peripheral_uuid=0x7f050006;
-        public static final int preferences=0x7f050002;
-        public static final int test=0x7f050007;
-        public static final int title_activity_main=0x7f050001;
-        public static final int title_activity_settings=0x7f050004;
+        public static final int app_name=0x7f040000;
+        public static final int ble_tag=0x7f040004;
+        public static final int hello_world=0x7f040006;
+        public static final int history=0x7f040003;
+        public static final int preferences=0x7f040002;
+        public static final int title_activity_main=0x7f040001;
+        public static final int title_activity_settings=0x7f040005;
     }
     public static final class style {
         /** 
@@ -43,14 +43,10 @@ public final class R {
             backward-compatibility can go here.
         
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f050000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
-    }
-    public static final class xml {
-        public static final int pref_ble_tag=0x7f040000;
-        public static final int pref_notification=0x7f040001;
+        public static final int AppTheme=0x7f050001;
     }
 }
--- BLETagNotifier/src/jp/nita/bletagnotifier/MainActivity.java
+++ BLETagNotifier/src/jp/nita/bletagnotifier/MainActivity.java
@@ -3,11 +3,8 @@ package jp.nita.bletagnotifier;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.Button;
 
 public class MainActivity extends Activity implements OnClickListener {
 
