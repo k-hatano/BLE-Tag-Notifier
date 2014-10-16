80141925be521f8108700c0294f6489b7cc00b47
k-hatano <kent.ruffle.mgj626@gmail.com>
2014-10-16 15:47:34 +0900

    中途半端に設定画面を作る

--- BLETagNotifier/gen/jp/nita/bletagnotifier/R.java
+++ BLETagNotifier/gen/jp/nita/bletagnotifier/R.java
@@ -15,9 +15,9 @@ public final class R {
     }
     public static final class id {
         public static final int history=0x7f060000;
-        public static final int listView=0x7f060003;
         public static final int preferences=0x7f060001;
         public static final int scrollView1=0x7f060002;
+        public static final int settings_items=0x7f060003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -26,11 +26,13 @@ public final class R {
     public static final class string {
         public static final int app_name=0x7f040000;
         public static final int ble_tag=0x7f040004;
-        public static final int hello_world=0x7f040006;
+        public static final int bluetooth_unavailable=0x7f040008;
+        public static final int hello_world=0x7f040007;
         public static final int history=0x7f040003;
         public static final int preferences=0x7f040002;
         public static final int title_activity_main=0x7f040001;
-        public static final int title_activity_settings=0x7f040005;
+        public static final int title_activity_settings=0x7f040006;
+        public static final int unregistered=0x7f040005;
     }
     public static final class style {
         /** 
--- BLETagNotifier/src/jp/nita/bletagnotifier/SettingsActivity.java
+++ BLETagNotifier/src/jp/nita/bletagnotifier/SettingsActivity.java
@@ -1,13 +1,63 @@
 package jp.nita.bletagnotifier;
 
-import android.app.Activity;
-import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-public class SettingsActivity extends Activity {
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
+
+public class SettingsActivity extends Activity implements OnItemClickListener {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_settings);
+		
+		ListView items=(ListView)findViewById(R.id.settings_items);
+		List<Map<String,String>> list=new ArrayList<Map<String,String>>();
+		{
+			Map<String,String> map;
+
+			map=new HashMap<String,String>();
+			map.put("key", getString(R.string.ble_tag));
+			map.put("value", getString(R.string.unregistered));
+			list.add(map);
+		}
+
+		SimpleAdapter adapter
+		=new SimpleAdapter(this,list
+				,android.R.layout.simple_expandable_list_item_2,
+				new String[]{"key","value"},
+				new int[]{android.R.id.text1,android.R.id.text2});
+		items.setAdapter(adapter);
+
+		items.setOnItemClickListener(this);
 	}
+
+	@Override
+	public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+		if(arg1==findViewById(R.id.settings_items)){
+			switch(arg2){
+			case 0:{
+				BluetoothAdapter bta = BluetoothAdapter.getDefaultAdapter();
+				if(bta==null){
+					Toast.makeText(this, getString(R.string.bluetooth_unavailable), Toast.LENGTH_LONG).show();
+					return;
+				}
+				break;
+			}
+			}
+		}
+	}
+	
+	
 }
